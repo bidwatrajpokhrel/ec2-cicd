5b9a8f1081c3fc69866b0df84a881fe07c971266
Bidwat Pokhrel <=>
2022-11-07 07:22:06 +0545

    ec2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CICD is working in EC2.</p>
+        <p>QUESTION MARK?!!.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
